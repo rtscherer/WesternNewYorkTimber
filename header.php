1d18f8852156285b790bfc0c4e9d1259955fcef1
Scherzi <rtscherer@gmail.com>
2014-10-13 20:46:55 -0400

    Adding .php shell pages

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php $page_title = $page_title ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $page_title ?></title>
+		
+	<link rel="stylesheet" href="styles/style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <ul>
+                <li>
+                    <a href="#">
+                        <div class="nav_button">
+                            Contact
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="nav_button">
+                            Services
+                        </div>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="#">
+                        <div class="nav_button">
+                            About Us
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="headertitle">Western New York Timber</div>
+    </header>
---
+++ main.php
@@ -0,0 +1,10 @@
+<main>
+	<div class="maintitle">
+    	We'd love your business!
+	</div>
+	<a href="#">
+		<div class="mainbutton">
+			Get a Quote!
+		</div>
+	</a>
+</main>
